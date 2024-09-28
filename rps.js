3f592feb9550fae1bb845b712074bc9a15b6a409
josephmara12 <71110479+josephmara12@users.noreply.github.com>
2024-09-28 17:13:17 +1000

    Working js for spr game

--- rps.js
+++ rps.js
@@ -1,66 +1,15 @@
 let playerWin = 0
 let comWin = 0
-
-function getHumanChoice() {
-    let select = prompt('Chose Rock Paper or Scissors')
-    firstlet = select.charAt(0)
-    capfirst = firstlet.toUpperCase()
-    rest = select.slice(1)
-    goodword = (capfirst + rest)
-    //console.log(goodword)
-
-    if ((goodword == 'Rock') ||( goodword == 'Paper') || (goodword == 'Scissors')) {
-        return goodword
-    } else {
-        alert('Not a valid choice') 
-    }
-
-}
+const buttons = document.querySelectorAll("button");
+let playerChoice = ''
 
 
-function getComputerChoice(min, max) {
-    comChoice = ''
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    num = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-    //console.log(num)
+function play() {
+    const humanSelection = playerChoice
+    const computerSelection = comChoice;
 
-    
-    switch (num) {
-        case 1:
-            comChoice = "Rock"
-            break;
-            
-        
-        case 2:
-            comChoice = "Paper"
-            break;
-            
-            
-        
-        case 3:
-            comChoice = "Scissors"
-            break;
-            
-    }
-
-    return comChoice
-    
-}
-
-getComputerChoice(1, 3)
-
-
-
-
-function playRound() {
-
-    
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice(1, 3);
-
-    console.log(humanSelection)
-    console.log(computerSelection)
+    console.log(humanSelection);
+    console.log(computerSelection);
 
     if (humanSelection == computerSelection) {
         console.log ('Tie')
@@ -98,13 +47,52 @@ function playRound() {
 
     }
 
-
-
-
-
-
-
 }
 
-console.log(`Human has won ${playerWin} times`)
-console.log(`Computer has won ${comWin} times`)
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playerChoice = button.textContent; 
+        play();
+        getComputerChoice(1, 3); 
+    });
+});
+
+
+
+function getComputerChoice(min, max) {
+    comChoice = ''
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    num = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+    //console.log(num)
+
+    
+    switch (num) {
+        case 1:
+            comChoice = "Rock"
+            break;
+            
+        
+        case 2:
+            comChoice = "Paper"
+            break;
+            
+            
+        
+        case 3:
+            comChoice = "Scissors"
+            break;
+            
+    }
+
+    return comChoice
+    
+}
+
+getComputerChoice(1, 3)
+
+
+
+
